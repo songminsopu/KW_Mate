0c8f850b25a27254115f30d73fcce867c1343281
Minsub Song <minsub1031@gmail.com>
2024-06-01 16:44:01 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'database.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SESSION_TYPE = 'filesystem'
